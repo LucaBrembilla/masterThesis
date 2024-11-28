e7bc6b8713b449c7fdb1c423d30fa627c6d2fe2a
bre <lucabremby00@gmail.com>
2024-11-28 17:47:04 +0100

    Remove open3d

--- tools/run_demo.sh
+++ tools/run_demo.sh
@@ -2,8 +2,8 @@
 
 CGF_FILE=cfgs/kitti_models/pv_rcnn.yaml
 CKPT=/home/brembilla/exp/pretrained/pv_rcnn_8369.pth
-POINT_CLOUD_DATA=/home/brembilla/exp/shared_datasets/kitti/data/sync/2011_10_03/2011_10_03_drive_0027_sync/velodyne_points/data/0000000541.bin
-
+#POINT_CLOUD_DATA=/home/brembilla/exp/shared_datasets/kitti/data/sync/2011_10_03/2011_10_03_drive_0027_sync/velodyne_points/data/0000000541.bin
+POINT_CLOUD_DATA=/home/brembilla/exp/models/pointpillars/dataset/demo_data/val/000134.bin
 # cd ..
 
 # pip install spconv-cu102

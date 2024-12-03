9e5f457f648860824303dcefe416fc0adf737670
bre <lucabremby00@gmail.com>
2024-12-03 10:40:53 +0100

    Fix spconv version

--- pcdet/utils/spconv_utils.py
+++ pcdet/utils/spconv_utils.py
@@ -1,7 +1,9 @@
 from typing import Set
 
-import spconv
-if float(spconv.__version__[2:]) >= 2.2:
+import spconv as sp
+import spconv.pytorch as spconv
+
+if float(sp.__version__[2:]) >= 2.2:
     spconv.constants.SPCONV_USE_DIRECT_TABLE = False
     
 try:

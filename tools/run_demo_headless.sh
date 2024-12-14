87c65beca9c0c2afa7246d1b41ca0e983731cd92
bre <lucabremby00@gmail.com>
2024-12-14 16:47:54 +0100

    Update

---
+++ tools/run_demo_headless.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+
+CGF_FILE=cfgs/kitti_models/pv_rcnn.yaml
+CKPT=/home/brembilla/exp/pretrained/pv_rcnn_8369.pth
+# POINT_CLOUD_DATA=/home/brembilla/exp/shared_datasets/kitti/kitti/training/velodyne/000063.bin
+POINT_CLOUD_DATA=/home/brembilla/exp/data/frame_1693_filtered.npy
+# cd ..
+
+# pip install spconv-cu102
+
+# python setup.py develop
+
+# cd tools
+
+# pip3 install --ignore-installed PyYAML
+
+# pip install open3d
+
+# python -m pcdet.datasets.kitti.kitti_dataset create_kitti_infos tools/cfgs/dataset_configs/kitti_dataset.yaml
+
+python demo_headless.py --cfg_file ${CGF_FILE} \
+    --ckpt ${CKPT} \
+    --data_path ${POINT_CLOUD_DATA}

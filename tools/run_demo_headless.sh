42f1b52ab63a9e997464c3fc40cda313c5a3b619
bre <lucabremby00@gmail.com>
2024-12-16 23:21:31 +0100

    Update

--- tools/run_demo_headless.sh
+++ tools/run_demo_headless.sh
@@ -1,11 +1,27 @@
 #!bin/bash
 
-# CGF_FILE=cfgs/kitti_models/pv_rcnn.yaml
+CGF_FILE=cfgs/kitti_models/pv_rcnn.yaml
+CGF_FILE=/home/brembilla/exp/tools/cfgs/kitti_models/pv_rcnn_plusplus_reproduced_by_community.yaml
 CGF_FILE=/home/brembilla/exp/tools/cfgs/kitti_models/pointrcnn.yaml
-# CKPT=/home/brembilla/exp/pretrained/pv_rcnn_8369.pth
+CGF_FILE=/home/brembilla/exp/tools/cfgs/kitti_models/pointpillar.yaml
+CGF_FILE=/home/brembilla/exp/tools/cfgs/kitti_models/second.yaml
+CGF_FILE=/home/brembilla/exp/tools/cfgs/kitti_models/voxel_rcnn_car.yaml
+CGF_FILE=/home/brembilla/exp/tools/cfgs/kitti_models/second_iou.yaml
+
+
+CKPT=/home/brembilla/exp/pretrained/pv_rcnn_8369.pth
 CKPT=/home/brembilla/exp/pretrained/pointrcnn_7870.pth
+CKPT=/home/brembilla/exp/pretrained/pointpillar_7728.pth
+CKPT=/home/brembilla/exp/pretrained/second_7862.pth
+CKPT=/home/brembilla/exp/pretrained/voxel_rcnn_car_84.54.pth
+CKPT=/home/brembilla/exp/pretrained/second_iou7909.pth
+
+
+
 POINT_CLOUD_DATA=/home/brembilla/exp/shared_datasets/kitti/kitti/testing/velodyne/000001.bin
-# POINT_CLOUD_DATA=/home/brembilla/exp/data/zeroed_frame_0000.npy
+POINT_CLOUD_DATA=/home/brembilla/exp/shared_datasets/PNRR/ouster/frame_0250.npy
+POINT_CLOUD_DATA=/home/brembilla/exp/data/zeroed_frame_0250.npy
+# POINT_CLOUD_DATA=/home/brembilla/exp/shared_datasets/PNRR/ouster/frame_2121.npy
 # cd ..
 
 # pip install spconv-cu102

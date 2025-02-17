5991d3019ea17b367dc361003c1219089c93142f
bre <lucabremby00@gmail.com>
2025-02-17 17:45:11 +0100

    Update

--- tools/pointpillar_test.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-CGF_FILE=cfgs/kitti_models/pointpillar.yaml
-CKPT=/home/brembilla/exp/pretrained/pointpillar_7728.pth
-BATCH_SIZE=2
-
-python test.py --cfg_file ${CGF_FILE} --batch_size ${BATCH_SIZE} --ckpt ${CKPT}
